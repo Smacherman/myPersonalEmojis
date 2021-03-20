fffecffc6bf49c6cfa2ba51c31ed50b965708980
Ryan Moore <Smacherman@gmail.com>
2021-03-19 21:45:17 -0700

    Added Javascript

---
+++ index.js
@@ -0,0 +1,26 @@
+
+
+const myEmojis = ["👨‍💻", "⛷", "🍲"]
+const emojiContainer = document.getElementById("emoji-container")
+
+function renderEmojis() {
+    for (let i = 0; i < myEmojis.length; i++){
+        const emoji = document.createElement('span')
+        emoji.textContent = myEmojis[i]
+        emojiContainer.append(emoji)
+    }
+}
+
+renderEmojis()
+
+const pushBtn = document.getElementById("push-btn")
+pushBtn.addEventListener("click", function(){
+    const emojiInput = document.getElementById("emoji-input")
+    if (emojiInput.value){
+        myEmojis.push(emojiInput.value)
+        emojiInput.value = ""
+        emojiContainer.innerHTML = ""
+        renderEmojis()
+    }
+
+})
